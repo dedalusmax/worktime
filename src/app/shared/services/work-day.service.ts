a1df3b55ba96ea6de41ceaa42b5ea7766a2b3c35
Marko Posavec <mp2@ekobit.corp>
2018-08-06 09:59:26 +0200

    method changed

--- src/app/shared/services/work-day.service.ts
+++ src/app/shared/services/work-day.service.ts
@@ -19,7 +19,7 @@ export class WorkDayService extends HttpBaseService<WorkDay>{
   private workDaysUrl = 'api/workDays';
 
   getWorkDaysByDate(startDate: Date, endDate: Date): Observable<WorkDay[]> {
-    return this.http.get<WorkDay[]>(this.workDaysUrl).pipe(map(workDays => workDays.filter(workDay => workDay.date.getTime < endDate.getTime && workDay.date.getTime > startDate.getTime)),
+    return this.http.get<WorkDay[]>(this.workDaysUrl).pipe(map(workDays => workDays.filter(workDay => workDay.date.getTime() < endDate.getTime() && workDay.date.getTime() > startDate.getTime())),
       catchError(this.handleError<WorkDay[]>(`getWorkDays`)));
   }
   
