b9b634839b2dafe89037a8233f9c90b36c0f4f52
Filip Žmuk <fz@ekobit.corp>
2018-08-10 08:50:53 +0200

    Record row gruped

--- src/app/reports/daily-reports/daily-reports.component.ts
+++ src/app/reports/daily-reports/daily-reports.component.ts
@@ -19,7 +19,7 @@ export class DailyReportsComponent implements OnInit {
 
   dailyRecords: DailyReport[];
 
-  // rowGroupMetadata = [];
+  rowGroupingMetadata = [];
 
   headersCSV = [
     {
@@ -55,7 +55,10 @@ export class DailyReportsComponent implements OnInit {
         this.endDate = endDate;
         return this.reportService.getWorkRecordsInPeriod(startDate, endDate);
       })
-    ).subscribe(data => this.dailyRecords = data);
+    ).subscribe(data => {
+      this.dailyRecords = data;
+      this.updateRowGroupMetaData();
+    });
   }
 
   exportCSVFormat({data, field}) {
@@ -73,32 +76,47 @@ export class DailyReportsComponent implements OnInit {
     return data;
   }
 
-  /*
-  let-rowIndex="rowIndex"
-  *ngIf="rowGroupMetadata[rowIndex] != 0" [attr.rowspan]="rowGroupMetadata[rowIndex]"
+  recordDatesAreEqual(firstRecord: DailyReport, secondRecord: DailyReport): boolean {
+    const firstDate: Date = firstRecord.workdate;
+    const secondDate: Date = secondRecord.workdate;
 
-  updateRowGroupMetaData() {
-    this.rowGroupMetadata = [];
-    if (!this.workRecords || this.workRecords.length == 0)
+    if (firstDate.getFullYear() !== secondDate.getFullYear()) {
+      return false;
+    }
+
+    if (firstDate.getMonth() !== secondDate.getMonth()) {
+      return false;
+    }
+
+    if (firstDate.getDay() !== secondDate.getDay()) {
+      return false;
+    }
+
+    return true;
+  }
+
+  updateRowGroupMetaData(): void {
+    this.rowGroupingMetadata = [];
+    if (!this.dailyRecords || this.dailyRecords.length === 0) {
       return;
+    }
 
     let lastFound = 0;
 
-    for (let i = 0; i < this.workRecords.length; i++) {
-      const workRecord = this.workRecords[i];
-      if (i == 0) {
-        this.rowGroupMetadata[0] = 1;
+    for (let i = 0; i < this.dailyRecords.length; i++) {
+      const dailyRecord = this.dailyRecords[i];
+      if (i === 0) {
+        this.rowGroupingMetadata[0] = 1;
       } else {
-        if(workRecord.workDate.getTime() == this.workRecords[i-1].workDate.getTime()) {
-          this.rowGroupMetadata[lastFound]++;
-          this.rowGroupMetadata[i] = 0;
+        if (this.recordDatesAreEqual(dailyRecord, this.dailyRecords[i - 1])) {
+          this.rowGroupingMetadata[lastFound]++;
+          this.rowGroupingMetadata[i] = 0;
         } else {
           lastFound = i;
-          this.rowGroupMetadata[i] = 1;
+          this.rowGroupingMetadata[i] = 1;
         }
       }
     }
   }
-  */
 }
 
